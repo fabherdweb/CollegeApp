9067a9bc00d0b951957c1a4667947442ec10ca20
fabherdweb <116849469+fabherdweb@users.noreply.github.com>
2022-12-01 01:20:02 -0500

    CollegeAppV1.1

---
+++ app/src/main/java/com/fabian/collegeapp/RegistreActivity.java
@@ -0,0 +1,14 @@
+package com.fabian.collegeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registre);
+    }
+}
