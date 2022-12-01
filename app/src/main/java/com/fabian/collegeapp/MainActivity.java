6584e76ef3d5b6ea073d72c6a8ebf759a4a954ce
fabherdweb <116849469+fabherdweb@users.noreply.github.com>
2022-12-01 01:20:02 -0500

    CollegeAppV1.1

--- app/src/main/java/com/fabian/collegeapp/MainActivity.java
+++ app/src/main/java/com/fabian/collegeapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.fabian.collegeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTextViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewRegister=findViewById(R.id.TextViewRegister);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,RegistreActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
