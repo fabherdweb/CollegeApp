0a6722723e3562578dcfafd7778233a323517b65
fabherdweb <116849469+fabherdweb@users.noreply.github.com>
2022-12-07 00:00:43 -0500

    CollegeAppV2.0

--- app/src/main/java/com/fabian/collegeapp/EstudianteActivity.java
+++ app/src/main/java/com/fabian/collegeapp/EstudianteActivity.java
@@ -2,18 +2,15 @@ package com.fabian.collegeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
-    TextView mTextViewRegister;
+public class EstudianteActivity extends AppCompatActivity {
+
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
@@ -21,17 +18,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_estudiante);
         inicializarBD();
-
-        mTextViewRegister=findViewById(R.id.TextViewRegister);
-        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,RegistreActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     public void inicializarBD() {
--- app/src/main/java/com/fabian/collegeapp/RegistreActivity.java
+++ app/src/main/java/com/fabian/collegeapp/RegistreActivity.java
@@ -3,12 +3,29 @@ package com.fabian.collegeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistreActivity extends AppCompatActivity {
+
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registre);
+        inicializarBD();
+    }
+
+    public void inicializarBD() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference= firebaseDatabase.getReference();
+        Toast.makeText(this, "Inicializando Base de Datos", Toast.LENGTH_SHORT).show();
     }
 }
